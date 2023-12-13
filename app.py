303fd7dd350356b3c2fea6c184a74226bf7d1c90
Revo Putera Samudera <revosamudera@gmail.com>
2023-12-13 18:36:31 +0700

    exercise_day_2_RevoSam

---
+++ app.py
@@ -0,0 +1,6 @@
+import module1
+
+fahrenheit = module1.celsius_to_fahr(20)
+kelvin = module1.fahr_to_kelvin(fahrenheit)
+
+print (kelvin)
---
+++ module1.py
@@ -0,0 +1,6 @@
+def celsius_to_fahr(temp):
+    return 9/5 * temp + 32
+
+def fahr_to_kelvin(temp):
+    return 9 * temp + 70
+
